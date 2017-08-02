e17abe20a235a749eb888381a380c46f9bfed4b8
Gajovski Maxime <gajovski.maxime@gmail.com>
2017-08-03 01:20:50 +0200

    [FEAT] - Creates method to transform csv into sql rows. Adds some usefull methods

--- cafe/utils/csv_utils.py
+++ cafe/utils/csv_utils.py
@@ -1,43 +1,59 @@
+import os
+
 from flask import current_app
 from numpy import genfromtxt
 
 from cafe import create_app
-from cafe.database.database import get_session
+from cafe.exceptions.file.file_format_exception import FileFormatException
+from cafe.exceptions.file.file_not_found_exception import FileNotFoundException
 from cafe.models.user import User
-from cafe.models.utils import table_name
-from cafe.utils.path_utils import get_csv_path_from_table_name
+from cafe.path_utils import get_folder_path_from_root_project
+from cafe.utils.models_utils import table_name
 
 
-# TODO verify if file exist.
-# TODO verify if it's csv file.
-# TODO verify if csv isn't malformed.
+def get_csv_path_from_table_name(table):
+    return get_folder_path_from_root_project('csv' + os.path.sep + table_name(table) + '.csv')
+
+
 def load_data_from_csv(file_name):
-    data = genfromtxt(file_name, delimiter=';', skip_header=1, dtype=None)
+    data = genfromtxt(
+        file_name,
+        skip_header=1,
+        missing_values='',
+        filling_values=0.0,
+        delimiter=',',
+        dtype=None)
     return data.tolist()
 
 
-def user_csv_to_sql():
+# TODO verify if csv isn't malformed.
+# TODO verify if file exist.
+# TODO verify if it's csv file.
+def user_csv_to_sql(session):
     with create_app().app_context():
-        session = get_session()
+        file_path = None
         try:
-
-            file_name = get_csv_path_from_table_name(User)
-            current_app.logger.info('Opening file : {}.'.format(file_name))
-            data = load_data_from_csv(file_name)
+            file_path = get_csv_path_from_table_name(User)
+            current_app.logger.info('Opening file : {}.'.format(file_path))
+            data = load_data_from_csv(file_path)
 
             for attr in data:
-                record = User(
-                    username=attr[0].decode('utf-8'),
-                    email=attr[1].decode('utf-8'),
-                    password=attr[2].decode('utf-8'),
-                    longitude=attr[3],
-                    latitude=attr[4])
-
+                record = User(**{
+                        'username':     attr[0].decode('utf-8'),
+                        'email':        attr[1].decode('utf-8'),
+                        'password':     attr[2].decode('utf-8'),
+                        'longitude':    float(attr[3]),
+                        'latitude':     float(attr[4])
+                })
                 session.add(record)
-            session.commit()
             current_app.logger.info('Rows inserted successfully in table {}.'.format(table_name(User)))
-        except OSError as err :
-            current_app.logger.error('Something wrong occurs in the database')
-            session.rollback()
-        finally:
-            session.close()
+
+        except (IndexError, ValueError) as err:
+            raise FileFormatException(
+                file_path=file_path,
+                err=err)
+        except OSError as err:
+            if '.csv not found' in err.args[0]:
+                raise FileNotFoundException(
+                    file_path=file_path,
+                    err=err)
--- cafe/utils/models_utils.py
+++ cafe/utils/models_utils.py
@@ -1,3 +1,7 @@
 
 def table_name(table):
     return table.__tablename__.lower()
+
+
+def column_name(table, key):
+    return table.properties.get(key)['name'].lower()
