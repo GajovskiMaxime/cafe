dd8496d7420c795208086e2d92505f8e96625e71
Gajovski Maxime <gajovski.maxime@gmail.com>
2017-08-03 01:17:01 +0200

    [FEAT] - Adds get_session method in database.py

--- cafe/database/database.py
+++ cafe/database/database.py
@@ -1,3 +1,14 @@
+from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
 
 db = SQLAlchemy()
+
+
+def get_session():
+    engine = db.get_engine(app=current_app)
+    Session = sessionmaker(
+        bind=engine,
+        autocommit=False,
+        expire_on_commit=False)
+    return Session()
